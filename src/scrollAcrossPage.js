454ca1928f37ab2f4a375132e6648ab824067424
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-05-19 12:36:36 +0200

    Adding all old Files

---
+++ src/scrollAcrossPage.js
@@ -0,0 +1,32 @@
+function scrollToAboutMe() {
+    const aboutMePage = document.querySelector('.aboutMePage');
+    aboutMePage.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
+function scrollToSkills() {
+    const skillsPage = document.querySelector('.skillsPage');
+    skillsPage.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
+function scrollToPortfolio() {
+    const portfolioPage = document.querySelector('.portfolioPage');
+    portfolioPage.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
+function downloadCV() {
+    console.log('Function under development');
+    alert('Function under development');
+}
+function scrollToContact() {
+    const contactPage = document.querySelector('.contactPage');
+    contactPage.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
